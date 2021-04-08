cb2c835d2cf36400c39b06f4003c53bbd9db22b5
jhuettinger <huettinger.johannes@outlook.de>
2021-04-08 16:21:55 +0200

    Detekt

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.sonarqube.gradle.SonarQubeTask
 
 val ktor_version: String by project
 val kotlin_version: String by project
@@ -8,10 +9,14 @@ val jacoco_version: String by project
 
 plugins {
     application
-    jacoco
     kotlin("jvm") version "1.4.32"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.4.32"
+
+    // Analysis & Reports
     id("org.jetbrains.dokka") version "1.4.30"
+    jacoco
+    id("io.gitlab.arturbosch.detekt") version "1.16.0"
+    id("org.sonarqube") version "3.1.1"
 }
 
 group = "net.bmgames"
@@ -40,24 +45,12 @@ dependencies {
     implementation("org.webjars:jquery:3.2.1")
 
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation ("io.kotest:kotest-runner-junit5:$kotest_version")
-    testImplementation ("io.kotest:kotest-assertions-core-jvm:$kotest_version")
-    testImplementation ("io.kotest:kotest-property-jvm:$kotest_version")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotest_version")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotest_version")
+    testImplementation("io.kotest:kotest-property-jvm:$kotest_version")
     testImplementation("io.kotest:kotest-assertions-ktor:$kotest_version")
 
 
-}
-
-tasks.withType<JacocoReport>{
-    reports {
-        xml.isEnabled = true
-        html.isEnabled = false
-    }
-}
-
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
@@ -66,4 +59,26 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "1.8"
     }
 }
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<JacocoReport> {
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = false
+    }
+}
+
+sonarqube {
+    properties {
+        property ("sonar.projectKey", "bm-games_BM-MUD")
+        property ("sonar.organization", "bm-games")
+        property ("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+
+
 
