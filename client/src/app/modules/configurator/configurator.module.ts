bd4a165fe319f91c2342844be045769d8afcefb7
2000DS <danielxscherer@live.de>
2021-04-21 17:54:00 +0200

    Added directory structure

---
+++ client/src/app/modules/configurator/configurator.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfigurationComponent } from './components/configuration/configuration.component';
+
+
+
+@NgModule({
+  declarations: [
+    ConfigurationComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConfiguratorModule { }
