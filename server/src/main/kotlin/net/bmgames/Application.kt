677ba75de4cd01e41764bf41768cd2dc86fe9dde
Maderu025 <maderu025@gmail.com>
2021-04-24 19:21:55 +0200

    Added final methods to authentication, Unit-Tests tbd

--- server/src/main/kotlin/net/bmgames/Application.kt
+++ server/src/main/kotlin/net/bmgames/Application.kt
@@ -3,18 +3,20 @@ package net.bmgames
 import net.bmgames.authentication.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import net.bmgames.Communication.MailNotifier
 import net.bmgames.plugins.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
+        embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+            configureRouting()
+            configureSecurity()
+            configureMonitoring()
+            configureSerialization()
+            configureSockets()
+        }.start(wait = true)
 
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureRouting()
-        configureSecurity()
-        configureMonitoring()
-        configureSerialization()
-        configureSockets()
-    }.start(wait = true)
 
+    }
 
-}
--- server/src/main/kotlin/net/bmgames/authentication/UserHandler.kt
+++ server/src/main/kotlin/net/bmgames/authentication/UserHandler.kt
@@ -1,50 +1,101 @@
 package net.bmgames.authentication
 
 import net.bmgames.Communication.MailNotifier
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 
-class UserHandler {
+public class UserHandler {
 
-   /* protected fun getUser(token: JWT): User?{
+
+    /* protected fun getUser(token: JWT): User?{
         return null
     }*/
-    val mailNotifier: MailNotifier = MailNotifier()
-    var currentUser: User? = null
 
-    fun getUserByName(username: String): User?{
-        return null
-    }
-    fun getUserByMail(mail: String): User?{
-        return null
-    }
+        val mailNotifier: MailNotifier = MailNotifier()
+        var currentUser: User? = null
 
-    public fun createUser(user: User) {
-        if (checkRegisterPossible(user.email, user.username)) {
+        fun getUserByName(username: String): User? {
+            var returnedUser: User? = null
+            transaction(/*später DB von bigBoyJohannes*/) {
+                usertable.select() {
+                    usertable.username eq username
+                }.forEach {
+                    returnedUser = User(
+                        email = it[usertable.email],
+                        username = it[usertable.username],
+                        passwordHash = it[usertable.passwordHash],
+                        mailVerified = it[usertable.mailVerified],
+                        registrationKey = ""
+                    )
+                }
+            }
+            return returnedUser
+        }
+
+        fun getUserByMail(mail: String): User? {
+            var returnedUser: User? = null
+            transaction(/*später DB von bigBoyJohannes*/) {
+                usertable.select() {
+                    usertable.email eq mail
+                }.forEach {
+                    returnedUser = User(
+                        email = it[usertable.email],
+                        username = it[usertable.username],
+                        passwordHash = it[usertable.passwordHash],
+                        mailVerified = it[usertable.mailVerified],
+                        registrationKey = ""
+                    )
+                }
+            }
+            return returnedUser
+        }
+
+        public fun createUser(user: User) {
             AuthHelper.generateVerificationToken(user)
-            //WriteOnDB /tbd
+            transaction {
+                usertable.insert {
+                    it[email] = user.email
+                    it[username] = user.username
+                    it[passwordHash] = user.passwordHash
+                    it[mailVerified] = user.mailVerified
+                }
+            }
             mailNotifier.sendMailRegister(user)
         }
-    }
 
-    fun resetPassword(mail: String){
-        val userByMail = getUserByMail(mail)
-        if (userByMail != null)
-        {
-            val password = AuthHelper.generatePassword()
-            changePassword(mail, password)
-            mailNotifier.sendMailReset(userByMail)
+
+        fun resetPassword(mail: String) {
+            val userByMail = getUserByMail(mail)
+            if (userByMail != null) {
+                val password = AuthHelper.hashPassword(AuthHelper.generatePassword())
+                changePassword(mail, password)
+                mailNotifier.sendMailReset(userByMail)
+            }
+
+        }
+
+        fun changePassword(mail: String, password: String) {
+            transaction {
+                usertable.update({usertable.email eq mail}){
+                    it[usertable.passwordHash] = password
+                }
+            }
+
+        }
+
+        public fun setMailApproved(mail: String) {
+            transaction {
+               usertable.update({usertable.email eq mail}) {
+                   it[usertable.mailVerified] = true
+               }
+            }
+        }
+
+        public fun checkRegisterPossible(email: String, username: String): Boolean {
+            val userByMail = getUserByMail(email)
+            val userByName = getUserByName(username)
+            if (userByMail != null || userByName != null)
+                return true
+            return false
         }
     }
-
-    fun changePassword(mail: String, password: String){
-        //DBZugriff
-
-    }
-
-    public fun setMailApproved(mail: String){
-
-    }
-
-    public fun checkRegisterPossible(email: String, username: String) : Boolean{
-        return false
-    }
-}
