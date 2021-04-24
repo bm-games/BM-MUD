e3a2075fdeeaa1fc5e75884ad8264e9966d8a622
el-presidente-kukki <armbruster.marius@t-online.de>
2021-04-24 21:15:41 +0200

    Added Hashing, Unit Test for Hashing and KDOC to all changes in this Branch

--- server/src/main/kotlin/net/bmgames/Communication/MailNotifier.kt
+++ server/src/main/kotlin/net/bmgames/Communication/MailNotifier.kt
@@ -6,7 +6,19 @@ import javax.mail.*
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
 
+/**
+ * Mail notifier
+ *
+ * @constructor Create empty Mail notifier
+ */
 class MailNotifier : Notifier {
+    /**
+     * Sends a mail to the user using the supplied parameters via smtp.
+     *
+     * @param recipient EMail address of the receiver/recipient.
+     * @param message Message in HTML format which will be sent to the recipient.
+     * @param mailSubject Self-explanatory
+     */
     override fun send(recipient: String, message: String, mailSubject: String) {
         val emailFrom = "support@bm-games.net"
         val properties = System.getProperties()
@@ -32,6 +44,11 @@ class MailNotifier : Notifier {
         Transport.send(mimeMessage)
     }
 
+    /**
+     * Sends(Generates) the user a mail with a generated password so that the user can use it to assign a new password.
+     *
+     * @param user
+     */
     fun sendMailReset(user: User) {
         val mailSubject = "Reset your password | BM-Games |  "
         val recipient: String = user.email
@@ -40,6 +57,12 @@ class MailNotifier : Notifier {
 
         send(recipient, message, mailSubject)
     }
+
+    /**
+     * Sends(Generates) a registration email to the user so that he can complete his registration using a generated code.
+     *
+     * @param user
+     */
     fun sendMailRegister(user: User) {
         val mailSubject = "Registration confirmation | BM-Games | SWE-Project  "
         val recipient: String = user.email
--- server/src/main/kotlin/net/bmgames/authentication/AuthHelper.kt
+++ server/src/main/kotlin/net/bmgames/authentication/AuthHelper.kt
@@ -1,7 +1,14 @@
 package net.bmgames.authentication
+
+import java.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
 const val KEY_LENGTH = 10
 const val PASSWORD_LENGTH = 20
 class AuthHelper {
+
 
     companion object{
         fun generateVerificationToken(user: User) {
@@ -10,17 +17,32 @@ class AuthHelper {
             val token = randomToken()
             user.registrationKey = token
         }
-
+        /**
+         * Generates a new random password
+         *
+         *
+         */
         fun generatePassword() : String {
             val chars = ('a'..'Z') + ('A'..'Z') + ('0'..'9')
             fun randomToken(): String = List(PASSWORD_LENGTH) { chars.random() }.joinToString("")
             return randomToken()
         }
-
+        /**
+         * Hashes the password of the user
+         *
+         * @Param password of the user
+         * @Return hashed version of the user's password
+         */
         fun hashPassword(password: String) : String {
-            return ""
+            val encrypted = HashingObject.cipher(Cipher.ENCRYPT_MODE, SECRET_KEY).doFinal(password.toByteArray(Charsets.UTF_8))
+            return String(HashingObject.encorder.encode(encrypted))
         }
-
+        /**
+         * Unhashes the password of the user
+         *
+         * @Param hashedpassword of the user
+         * @return unhashed version of the hashed password
+         */
         fun unhashPassword(hashedpassword: String) : String {
             return ""
         }
