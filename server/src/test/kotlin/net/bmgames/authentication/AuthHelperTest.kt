080cbc0de5ae47e269c6bcf7e98b8fcf95800ec5
jhuettinger <huettinger.johannes@outlook.de>
2021-05-04 11:52:00 +0200

    Created Auth branch

--- server/src/test/kotlin/net/bmgames/authentication/AuthHelperTest.kt
+++ server/src/test/kotlin/net/bmgames/authentication/AuthHelperTest.kt
@@ -4,20 +4,16 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import net.bmgames.DEMO_CONFIG
 
-/*
 class AuthHelperTest : FunSpec({
-    val authHelper = AuthHelper(DEMO_CONFIG.copy(secretKeyHash = "R6A6QCo5oTygj37aM4zG"))
-
 
     xtest("Hashed password should be: Ux1sgq1OagEn5G77TLpUCA== ") {
-        val test = authHelper.hashPassword("password")
+        val test = AuthHelper.hashPassword("password")
         test shouldBe "Ux1sgq1OagEn5G77TLpUCA=="
     }
 
     xtest("Unhashed password should be: Gurkensalat") {
-        val test = authHelper.unhashPassword("ohtBU7hSCvmQqZGbIkoFKg==")
+        val test = AuthHelper.unhashPassword("ohtBU7hSCvmQqZGbIkoFKg==")
         test shouldBe "Gurkensalat"
     }
 
 })
-*/
