221c2f84d62bfbfc5da618239f19dff702c6c62a
jhuettinger <huettinger.johannes@outlook.de>
2021-05-06 10:07:06 +0200

    Changed model

--- server/src/main/kotlin/net/bmgames/state/ConfigEndpoint.kt
+++ server/src/main/kotlin/net/bmgames/state/ConfigEndpoint.kt
@@ -3,6 +3,8 @@
 package net.bmgames.state
 
 import arrow.core.Either
+import arrow.core.computations.either
+import arrow.core.rightIfNotNull
 import arrow.core.rightIfNull
 import arrow.core.toOption
 import io.ktor.application.*
@@ -16,8 +18,8 @@ import net.bmgames.ErrorMessage
 import net.bmgames.state.model.Game
 
 internal class ConfigEndpoint {
-    fun saveConfig(config: Game): Either<ErrorMessage, Unit> = TODO()
-    fun getConfig(name: String): Game? = TODO()
+    fun saveConfig(config: DungeonConfig): Either<ErrorMessage, Unit> = TODO()
+    fun getConfig(name: String): DungeonConfig? = TODO()
 }
 
 fun Route.installConfigEndpoint() {
@@ -27,14 +29,18 @@ fun Route.installConfigEndpoint() {
             configEndpoint.getConfig(name)
                 ?.let { config -> call.respond(config) }
                 ?: call.respond(HttpStatusCode.BadRequest)
+        }
+        post("/createConfig") {
+            either<ErrorMessage, Unit> {
+                val config = call.receive<DungeonConfig>().rightIfNotNull { "Config missing" }.bind()
+                println(config)
+                configEndpoint.saveConfig(config).bind()
+            }.fold(
+                { error -> call.respond(HttpStatusCode.BadRequest, error) },
+                { call.respond(HttpStatusCode.Accepted) }
+            )
+        }
     }
-    post("/createConfig") {
-        val config = call.receive<Game>()
-        println(config)
-        TODO("Validate")
-        call.respond(HttpStatusCode.Accepted)
-    }
-}
 }
 
 @Location("/get/{name}")
