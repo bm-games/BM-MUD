ab202bcf53b25dacf229ce5c218d5a13ec1f107d
Maderu025 <maderu025@gmail.com>
2021-05-13 13:05:14 +0200

    added MoveCommand.toAction()

--- server/src/main/kotlin/net/bmgames/game/commands/player/MoveCommand.kt
+++ server/src/main/kotlin/net/bmgames/game/commands/player/MoveCommand.kt
@@ -1,12 +1,19 @@
 package net.bmgames.game.commands.player
 
 import arrow.core.Either
+import arrow.core.computations.either
+import arrow.core.left
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.enum
+import net.bmgames.errorMsg
 import net.bmgames.game.action.Action
+import net.bmgames.game.action.MoveAction
 import net.bmgames.game.commands.PlayerCommand
+import net.bmgames.game.commands.getRoom
 import net.bmgames.state.model.Game
 import net.bmgames.state.model.Player
+import net.bmgames.state.model.Room
+import net.bmgames.success
 
 /**
  * eg. move north
@@ -16,13 +23,31 @@ class MoveCommand : PlayerCommand("move") {
     enum class Direction { NORTH, EAST, SOUTH, WEST }
     val direction: Direction by argument(help = "The direction you want to move to").enum()
 
-    override fun toAction(player: Player.Normal, game: Game): Either<String, List<Action>> {
-        TODO("Rooms not yet implemented")
-        /*val playerRoom : Room
-        when(direction){
-            Direction.NORTH -> game. Logik zur Berechnung der Räume
-            Direction.EAST -> game. Logik zur Berechnung der Räume
-            Direction.WEST -> game. Logik zur Berechnung der Räume
-            Direction.SOUTH -> game. Logik zur Berechnung der Räume*/
+    override fun toAction(player: Player.Normal, game: Game): Either<String, List<Action>> = either.eager {
+        val room = player.getRoom(game).bind()
+        val actions = mutableListOf<Action>()
+        val destinationRoom : Room?;
+            when(direction) {
+                Direction.NORTH -> {
+                    destinationRoom = room.north?.let { game.getRoom(it) }
+                }
+                Direction.EAST -> {
+                    destinationRoom = room.east?.let { game.getRoom(it) }!!
+                }
+                Direction.WEST -> {
+                    destinationRoom = room.west?.let { game.getRoom(it) }!!
+                }
+                Direction.SOUTH -> {
+                    destinationRoom = room.south?.let { game.getRoom(it) }!!
+                }
+            }
+        if(destinationRoom == null)
+            errorMsg("Destination does not exist")
+        else{
+            actions.add(MoveAction(player.left(), room, destinationRoom))
+            success(actions)
+        }.bind()
     }
+
 }
+
