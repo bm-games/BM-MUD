13e771c860ab61fc0cce1bb645e962091b07e3cb
Maderu025 <maderu025@gmail.com>
2021-05-14 14:55:42 +0200

    remade avatar creator based on dungeon config

--- client/src/app/avatarconfigurator/components/avatarconfigurator.component.ts
+++ client/src/app/avatarconfigurator/components/avatarconfigurator.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DungeonConfig} from '../../configurator/models/DungeonConfig';
+import {RaceConfig} from '../../configurator/models/RaceConfig';
+import {ClassConfig} from '../../configurator/models/ClassConfig';
 
 @Component({
   selector: 'app-avatarconfigurator',
@@ -7,13 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvatarconfiguratorComponent implements OnInit {
 
-  isOrk: boolean = true;
-  isElf: boolean = false;
-  isBerserker: boolean = true;
-  isArcher: boolean = false;
 
-  damage: number = 0;
-  health: number = 0;
+  currentConfig: DungeonConfig; //=getConfigOrWhatEverNichtMeinJobDigga;~Leon, 2021;  @Jakub ~Marius, 2021
+  raceConfigs: RaceConfig[] = this.currentConfig.races;
+  classConfigs: ClassConfig[] = this.currentConfig.classes;
+
+  damage = 0;
+  health = 0;
 
   rawdamage: number | undefined;
   rawhealth: number | undefined;
@@ -21,68 +24,72 @@ export class AvatarconfiguratorComponent implements OnInit {
   damageMultiplier: number | undefined;
   healthMultiplier: number | undefined;
 
-  selectedRace: string = "Ork";
-  selectedClass: string = "Berserker";
+  selectedRace = '';
+  selectedClass = '';
 
-  classTypes: string[] = ["Berserker", "Bogenschütze"];
-  raceTypes: string[] = ["Ork", "Elf"]
+  classTypes: string[] = [];
+  raceTypes: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    this.raceChanged("Ork");
-    this.classChanged("Berserker");
+    //getDungeonConfigOderSoIGuess
+    this.getClassesAndRaces();
+    this.setRaceInitial();
+    this.setClassInitial();
+    this.calculateInitialStats();
+  }
+
+  setRaceInitial(): void {
+    this.selectedRace = this.raceConfigs[0].name;
+    this.rawhealth = this.raceConfigs[0].health;
+    this.damageMultiplier = this.raceConfigs[0].damageModifier;
+  }
+
+  setClassInitial(): void {
+    this.selectedClass = this.classConfigs[0].name;
+    this.healthMultiplier = this.classConfigs[0].healthMultiplier;
+    this.rawdamage = this.classConfigs[0].damage;
+  }
+
+  calculateInitialStats(): void{
+    if (this.rawhealth == undefined || this.rawdamage == undefined
+      || this.healthMultiplier == undefined || this.damageMultiplier == undefined) {
+      return;
+    }
+    this.health = this.rawhealth * this.healthMultiplier;
+    this.damage = this.damageMultiplier * this.rawdamage;
+  }
+
+  getClassesAndRaces(): void {
+    this.raceConfigs = this.currentConfig.races;
+    this.classConfigs = this.currentConfig.classes;
   }
 
   raceChanged(race: string) {
-    switch (race) {
-      case 'Ork':
-        this.isOrk = true;
-        this.isElf = false;
-        this.rawdamage = 10;
-        this.rawhealth = 100;
-        // @ts-ignore
-        this.damage = this.rawdamage * this.damageMultiplier;
-        // @ts-ignore
-        this.health = this.rawhealth * this.damageMultiplier;
-        break;
-      case 'Elf':
-        this.isElf = true;
-        this.isOrk = false;
-        this.rawdamage = 20;
-        this.rawhealth = 50;
-        // @ts-ignore
-        this.damage = this.rawdamage * this.damageMultiplier;
-        // @ts-ignore
-        this.health = this.rawhealth * this.damageMultiplier;
-        break;
+    for (let i = 0; i < this.raceConfigs.length; i++){
+      if (this.raceConfigs[i].name == race){
+        this.damageMultiplier = this.raceConfigs[i].damageModifier;
+        this.rawhealth = this.raceConfigs[i].health;
+        this.health = this.rawhealth * this.healthMultiplier;
+        this.selectedRace = this.raceConfigs[i].name;
+      }
     }
+    return;
   }
 
+
+
   classChanged(classType: string) {
-    switch (classType) {
-      case 'Berserker':
-        this.isBerserker = true;
-        this.isArcher = false;
-        this.healthMultiplier = 1.2;
-        this.damageMultiplier = 1.2;
-        // @ts-ignore
+    for (let i = 0; i < this.classConfigs.length; i++) {
+      if (this.classConfigs[i].name == classType){
+        this.rawdamage = this.classConfigs[i].damage;
+        this.healthMultiplier = this.classConfigs[i].healthMultiplier;
         this.damage = this.rawdamage * this.damageMultiplier;
-        // @ts-ignore
         this.health = this.rawhealth * this.healthMultiplier;
-        console.log("Berserker");
-        break;
-      case 'Bogenschütze':
-        this.isArcher = true;
-        this.isBerserker = false;
-        this.healthMultiplier = 0.8;
-        this.damageMultiplier = 1.6;
-        // @ts-ignore
-        this.damage = this.rawdamage * this.damageMultiplier;
-        // @ts-ignore
-        this.health = this.rawhealth * this.healthMultiplier;
-        break;
+      }
     }
+    return;
   }
 
 }
