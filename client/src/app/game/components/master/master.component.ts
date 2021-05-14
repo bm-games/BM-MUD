330b16ff14b8547b2ded08250d15d9b2467d5924
2000DS <67456742+2000DS@users.noreply.github.com>
2021-05-14 20:37:57 +0200

    Added methods for all master functionalities and connected them with the UI

--- client/src/app/game/components/master/master.component.ts
+++ client/src/app/game/components/master/master.component.ts
@@ -26,15 +26,19 @@ export class MasterComponent implements OnInit {
   selectedGridValueIndex: number = 0;
   selectedRoomName: string = '';
   selectedRoomMessage: string = '';
-  selectedRoomNPCs: NPC[] = [];
-  selectedRoomItems: Item[] = [];
-  selectedRoomValues: string[] = ['Stock', 'GeOrk', 'Stock', 'GeOrk', 'Stock', 'GeOrk'];
-  selectedRoomPlayers: string[] = [];
+  selectedRoomNPCs: NPC[] = [{ name: 'npc1', commandOnInteraction: '', messageOnTalk: 'hallo', items: [], type: "net.bmgames.state.model.NPC.Friendly"}, { name: 'npc2', health: 3, damage: 3, items: [], type: "net.bmgames.state.model.NPC.Hostile"}]
+  npcsToAdd: NPC[] = [];
+  selectedRoomItems: Item[] = []
+  itemsToAdd: Item[] = []
+  selectedRoomPlayers: string[] = ['Hans', 'Martin', 'Peter'];
+  selectedPlayerInRoom: string | undefined;
   onlinePlayers: string[] = ['Hans', 'Martin', 'Peter'];
   commandsOnPlayer: string[] = ['Spieler rauswerfen', 'Spieler teleportieren', 'Charakter LP abziehen']
+  playerToInviteName: string | undefined;
   selectedCommandOnPlayer!: string;
-  allDungeonNPCs: string[] = [];
-  allDungeonItems: string[] = [];
+  selectedRoomToTeleportTo!: string;
+  allDungeonNPCs: NPC[] = [];
+  allDungeonItems: Item[] = [];
   allDungeonRooms: string[] = [];
 
   disableNewRoomTab: boolean = true;
@@ -84,6 +88,138 @@ export class MasterComponent implements OnInit {
         color: "#C0C0C0"
       }
     }
+    this.allDungeonNPCs.push({ name: 'testtttt', commandOnInteraction: '', messageOnTalk: 'hallo', items: [], type: "net.bmgames.state.model.NPC.Friendly"})
+    this.allDungeonItems.push({ name: 'testtttt', effect: 'effect', type: "net.bmgames.state.model.Consumable"})
+    this.allDungeonItems.push({ name: 'fd', slot: 'Head', healthModifier: 2, damageModifier: 3, type: "net.bmgames.state.model.Equipment"})
+    this.allDungeonItems.push({ name: 'sdd', damage: 3, type: "net.bmgames.state.model.Weapon"})
+  }
+
+  /**
+   * Removes the selected NPC from the room. The NPC is still available in the list allDungeonNPCs
+   */
+  removeNPCFromRoom(){
+    console.log(this.selectedGridValueIndex)
+    //save this.selectedRoomNPCs to the room npcs
+    //TODO
+  }
+
+  /**
+   * Adds the selected NPCs to the selected room
+   */
+  addNPCToRoom(){
+    console.log(this.npcsToAdd)
+    this.npcsToAdd.forEach(n => {
+      if(!this.checkSelectedRoomContainsNPC(n.name)){
+        this.selectedRoomNPCs.push(n)
+      }
+    })
+    this.npcsToAdd = []
+
+    //save this.selectedRoomNPCs to the rooms npcs
+    //TODO
+  }
+
+  checkSelectedRoomContainsNPC(npcName: string) : boolean{
+    for (let i = 0; i < this.selectedRoomNPCs.length; i++) {
+      if(this.selectedRoomNPCs[i].name == npcName) return true
+    }
+    return false
+  }
+
+  checkSelectedRoomContainsItem(itemName: string) : boolean{
+    for (let i = 0; i < this.selectedRoomItems.length; i++) {
+      if(this.selectedRoomItems[i].name == itemName) return true
+    }
+    return false
+  }
+
+  /**
+   * Removes the selected NPC from the room. The NPC is still available in the list allDungeonNPCs
+   */
+  removeItemFromRoom(){
+    console.log(this.selectedGridValueIndex)
+    //save this.selectedRoomItems to the room items (send command)
+    //TODO
+  }
+
+  /**
+   * Adds the selected NPC to the selected room
+   */
+  addItemToRoom(){
+    console.log(this.itemsToAdd)
+    this.itemsToAdd.forEach(n => {
+      if(!this.checkSelectedRoomContainsItem(n.name)){
+        this.selectedRoomItems.push(n)
+      }
+    })
+    this.itemsToAdd = []
+
+    //save this.selectedRoomNPCs to the rooms items
+    //TODO
+  }
+
+  /**
+   * Sends a kick command to the server which kicks the player out of the dungeon
+   * @param playerName username of the player to kick out of the dungeon
+   */
+  kickPlayer(playerName?: string){
+    console.log(this.selectedPlayerInRoom)
+    //send kick command
+    //TODO
+  }
+
+  /**
+   * Sends a teleport command to the server which teleports the player to the selected room
+   */
+  teleportPlayer(){
+    console.log(this.selectedPlayerInRoom)
+    console.log(this.selectedRoomToTeleportTo)
+    //TODO
+  }
+
+  /**
+   * Hits a player and decrements his health
+   * @param damage the number healthpoints that are going to be subtracted from players health
+   */
+  hitPlayer(damage: number | null){
+    console.log(this.selectedPlayerInRoom)
+    console.log(damage)
+    //TODO
+  }
+
+  /**
+   * Deletes the selected room
+   * This is only possible, if the selected room doesn't contains any characters
+   */
+  deleteRoom(){
+    console.log(this.selectedGridValueIndex)
+    //TODO
+  }
+
+  /**
+   * Creates a new room and adds it at the selected position to the dungeon
+   * It sets the input values name, description, NPCs and Items
+   */
+  addNewRoom(){
+    console.log(this.selectedRoomName)
+    console.log(this.selectedRoomMessage)
+    console.log(this.selectedRoomNPCs)
+    console.log(this.selectedRoomItems)
+
+    this.selectedRoomName = ''
+    this.selectedRoomMessage = ''
+    this.selectedRoomNPCs = []
+    this.selectedRoomItems = []
+    //TODO
+  }
+
+  /**
+   * Sends a invitePlayer command to the server to send an invitation to the player
+   */
+  invitePlayer(){
+    console.log("Invite player: " + this.playerToInviteName)
+    this.playerToInviteName = undefined
+    //TODO
   }
 
   gridRoomSelected(gridV: gridValue){
