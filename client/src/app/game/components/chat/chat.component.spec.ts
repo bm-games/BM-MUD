9b3915b9ccdd19e89c825626b2cd1b366f15686e
jhuettinger <huettinger.johannes@outlook.de>
2021-05-17 10:37:09 +0200

    Release 0.4.0

--- client/src/app/game/components/chat/chat.component.spec.ts
+++ client/src/app/game/components/chat/chat.component.spec.ts
@@ -1,7 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ChatComponent } from './chat.component';
+import {ChatComponent} from './chat.component';
 import {Subject} from "rxjs";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('ChatComponent', () => {
   let component: ChatComponent;
@@ -9,9 +10,10 @@ describe('ChatComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChatComponent ]
+      declarations: [ChatComponent],
+      imports: [NoopAnimationsModule]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
