467fca8cbec33e6e69f1435d69b21ddf13999dda
el-presidente-kukki <armbruster.marius@t-online.de>
2021-05-18 17:42:17 +0200

    Edited baseUrl

--- client/e2e/protractor.conf.js
+++ client/e2e/protractor.conf.js
@@ -17,7 +17,7 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://play.bm-games.net',
+  baseUrl: 'http://localhost:4200/ ',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
